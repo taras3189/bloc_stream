3d76cdddee87fad3dd4a67b80b96eb29a881bd0a
Taras Bobriienko <taras3189@gmail.com>
2023-03-22 16:56:56 +0200

    Firs commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,76 @@
 import 'package:flutter/material.dart';
+import 'color_bloc.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Material App Bar'),
+    return const MaterialApp(
+      title: 'Flutter Demo',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final ColorBloc _bloc = ColorBloc();
+
+  @override
+  void dispose() {
+    _bloc.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('BLoC with Stream'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: StreamBuilder(
+          stream: _bloc.outputStateStream,
+          initialData: Colors.red,
+          builder: (context, AsyncSnapshot<Color> snapshot) {
+            return AnimatedContainer(
+              height: 100,
+              width: 100,
+              color: snapshot.data,
+              duration: const Duration(milliseconds: 500),
+            );
+          },
         ),
-        body: Center(
-          child: Container(
-            child: Text('Hello World'),
+      ),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          FloatingActionButton(
+            backgroundColor: Colors.red,
+            onPressed: () {
+              _bloc.inputEventSink.add(ColorEvent.eventRed);
+            },
           ),
-        ),
+          const SizedBox(width: 10),
+          FloatingActionButton(
+            backgroundColor: Colors.green,
+            onPressed: () {
+              _bloc.inputEventSink.add(ColorEvent.eventGreen);
+            },
+          ),
+        ],
       ),
     );
   }
